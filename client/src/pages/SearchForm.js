700b799116364d38f419799a86e8de9eaf74f7cd
Brandon King <brandonscottking@gmail.com>
2022-05-25 20:57:10 -0400

    added search page

--- client/src/pages/SearchForm.js
+++ client/src/pages/SearchForm.js
@@ -1,43 +1,30 @@
 import React from "react";
 import { Button } from '@mui/material'
 import Dropdown from "../components/Dropdown";
-import Input from "../components/Input";
+import ResultsContainer from "../components/ResultsContainer";
 
 const SearchForm = () => {
-
-  const questType = document.getElementById('type-select');
-  const questCat = document.getElementById('cat-select');
-  const question = document.getElementById('q-input');
-  const answer = document.getElementById('a-input');
-
-  function saveForm(a,b,c,d){
-    
-    const newItem = {
-      Type: a,
-      Category: b,
-      Question: c,
-      Answer: d
-    };
-
-    console.log('yesplease')
-  }
 
   const listOptions = [
     'One', 'TWO', 'Threee'
   ]
 
+  const listOptionsTwo = [
+    'Easy', 'Medium', 'Hard'
+  ]
   return (
     <>
-    <h1>This is a different page!</h1>
     <form action={console.log('data')}>
     <div id="input-div">
         <Dropdown label="Question Type" id="type-select" options={listOptions} />
         <Dropdown label="Category" id="cat-select" options={listOptions} />
-        <Input id="q-input" label="Question" />
-        <Input id="a-input" label="Answer" />
-        <Button variant="contained" onClick={saveForm(questType, questCat, question, answer)}>Submit</Button>
+        <Dropdown label="Difficulty" id="diff-select" options={listOptionsTwo} />
+        <Button variant="contained" onClick="{/* api call */}">Search</Button>
     </div>
     </form>
+    <div id="results">
+      <ResultsContainer />
+    </div>
     </>
   )
 }
