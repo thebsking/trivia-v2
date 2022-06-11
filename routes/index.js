1b62386a5fd4a457382b1a87921fb6f28ae2493b
Brandon King <brandonscottking@gmail.com>
2022-06-11 08:56:45 -0400

    add route

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,20 @@
 const router = require('express').Router();
-const {question} = require('../models')
+const { question } = require('../models')
 
 //Search API
 
 
 //Add API
 router.post('/add', async (req, res) => {
-  const newQuestion = await question.create({
+  try {
+    const newQuestion = await question.create({
     type: req.body.type,
     category: req.body.category,
     question: req.body.question,
     answer: req.body.answer,
   });
-})
+  res.status(200).json(newQuestion);
+} catch (err) {
+  res.status(400).json(err);
+}
+});
