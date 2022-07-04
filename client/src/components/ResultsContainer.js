58583063f9d560afc7d9ee7b2a4b86b84bcc143f
Brandon King <brandonscottking@gmail.com>
2022-07-04 16:25:39 -0500

    refactored location page to use api; created first api for search results

--- client/src/components/ResultsContainer.js
+++ client/src/components/ResultsContainer.js
@@ -1,8 +1,16 @@
 import React from "react";
 
-const ResultsContainer = () => {
-  return(
-    <h1>Results</h1>
+const ResultsContainer = ({ props }) => {
+  let array = props;
+
+  return (
+    array.map(x => {
+      return (<div>
+        <p>Category: {x.cateogry}</p>
+        <p>Question: {x.question}</p>
+        <p>Answer: {x.answer}</p>
+      </div>)
+    })
   )
 }
 
--- client/src/pages/SearchForm.js
+++ client/src/pages/SearchForm.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { Button, ButtonGroup } from '@mui/material'
+import React, { useState } from "react";
+import { Button, ButtonGroup, Card } from '@mui/material'
 import Dropdown from "../components/Dropdown";
 import ResultsContainer from "../components/ResultsContainer";
 import TopBar from '../components/TopBar';
+import axios from "axios";
 
 const SearchForm = () => {
 
@@ -10,26 +11,36 @@ const SearchForm = () => {
     'One', 'TWO', 'Threee'
   ]
 
-  const listOptionsTwo = [
-    'Easy', 'Medium', 'Hard'
-  ]
+  const [results, setResults] = useState([])
 
-  const catSelect = document.getElementById('cat-select');
+  const handleRandom = () => {
+    axios.get('/api/questions')
+      .then(res => setResults(res.data))
+      .catch(err => console.log(err))
+  }
 
   return (
     <>
       <TopBar />
-          <div id="input-div" className="center-div">
-            <Dropdown label="Category" id="cat-select" options={listOptions} />
-            <ButtonGroup variant="contained" orientation="vertical" >
-              <Button>Search by Category</Button>
-              <Button>Range Questions</Button>
-              <Button>Random</Button>
-            </ButtonGroup>
-          </div>
-        <div id="results">
-          <ResultsContainer />
-        </div>
+      <div id="input-div" className="center-div">
+        <Dropdown label="Category" id="cat-select" options={listOptions} />
+        <ButtonGroup variant="contained" orientation="vertical" >
+          <Button>Search by Category</Button>
+          <Button>Range Questions</Button>
+          <Button onClick={handleRandom}>Random</Button>
+        </ButtonGroup>
+      </div>
+      <div id="results">
+        {results.map(x => {
+          return (
+            <Card className="q-card">
+              <p>Category: {x.category}</p>
+              <p>Question: {x.question}</p>
+              <p>Answer: {x.answer}</p>
+            </Card>
+          )
+        })}
+      </div>
     </>
   )
 }
